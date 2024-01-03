86d094e3da704db353e579730569929967279f96
JeongHunHui <deca3248@gmail.com>
2024-01-02 23:48:12 -0800

    chore: 초기 환경 세팅

---
+++ src/main/kotlin/com/hunhui/websocket/WebsocketApplication.kt
@@ -0,0 +1,11 @@
+package com.hunhui.websocket
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsocketApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebsocketApplication>(*args)
+}
